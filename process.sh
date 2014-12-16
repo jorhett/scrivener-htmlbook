cd5124d246aadd0e02e3d1822260eb37cdaf685d
Jo Rhett <jrhett@netconsonance.com>
2014-12-16 00:20:16 -0800

    Get rid of the temp file when completed successfully.

--- process.sh
+++ process.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 SCRIPT_DIR=$(dirname $0)
 SCRIVENER_OUTPUT=$1
+PARAS_ADDED=$1.parsed
+
+# mark paragraphs
+${SCRIPT_DIR}/markParagraphs.pl $SCRIVENER_OUTPUT $PARAS_ADDED
 
 # Split the scrivener output into chapters
-${SCRIPT_DIR}/splitHTMLBook.pl $SCRIVENER_OUTPUT 
+${SCRIPT_DIR}/splitHTMLBook.pl $PARAS_ADDED 
 splitstatus=$?
 if [ $splitstatus -ne 0 ]; then
     echo "Last status was $splitstatus"
     exit $splitstatus
 fi 
+rm $PARAS_ADDED
